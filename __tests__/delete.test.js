a7accc08f6e98539084dface09b663265e9e6d37
AlexanderSaid <alexander.t.said@gmail.com>
2022-01-11 22:25:20 +0100

    edit deleteTest

--- __tests__/delete.test.js
+++ __tests__/delete.test.js
@@ -6,25 +6,29 @@ const request = supertest(app);
 //! Test DELETE /movies/:id
 describe("DELETE /movies/:id", () => {
   describe("User send a valid ID", () => {
-    it("Respond with a 201 status code and a confirmation message", async () => {
-      const id = "9791295e-97c4-49b9-ageb-dbb8aa2gd8so";
-      const res = await request.delete(`/movies/${id}`);
-
-      expect(res.statusCode).toBe(201);
-      expect(typeof res.body).toBe("object");
-      expect(res.body.msg).toEqual(
-        `The movie "The Irishman" has been removed.`
-      );
+    it("Respond with a 201 status code and a confirmation message", (done) => {
+      const validID = "9791295e-97c4-49b9-ageb-dbb8aa2gd8so";
+      request
+        .delete(`/movies/${validID}`)
+        .expect(201)
+        .expect((res) => {
+          typeof res.body === "object";
+          res.body.msg === `The movie "The Irishman" has been removed.`;
+        })
+        .end(done);
     });
   });
   describe("User send invalid ID", () => {
-    it("Respond with a 404 status code and an error message", async () => {
-      const id = "376502";
-      const res = await request.delete(`/movies/${id}`);
-
-      expect(res.statusCode).toBe(404);
-      expect(typeof res.body).toBe("object");
-      expect(res.body.msg).toEqual(`There is no movie with id ${id}`);
+    it("Respond with a 404 status code and an error message", (done) => {
+      const invalidID = "376502";
+      request
+        .delete(`/movies/${invalidID}`)
+        .expect(404)
+        .expect(res => {
+          typeof res.body === 'object';
+          res.body.msg === `There is no movie with id ${invalidID}`
+        })
+        .end(done);
     });
   });
 });
